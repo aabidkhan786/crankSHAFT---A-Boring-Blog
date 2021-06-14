fe404781397429bd022cf65da20242866f76dd03
Aabid <abidkhan786@outlook.com>
2021-06-14 22:21:41 +0530

    Update 3

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,8 +1,11 @@
+import LOGO from './android-chrome-512x512.png';
+
 
 const Navbar = () => {
     return ( 
         <nav className="navbar">
-            <div className="logo-section"><h1>CrankSHAFT - A Boring Blog</h1></div>
+            
+            <div className="logo-section"><img src={LOGO} alt="" className="logo"/><h1>CrankSHAFT - A Boring Blog</h1></div>
             <div className="link">
                 <a href="/">Home</a>
                 <a href="/create">New Blog</a>
